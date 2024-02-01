761d0e5f7dad58ca752b9bbe4ce8ea98fb963582
Jatin Khemchandani <Jatin.Khemchandani@preqin.com>
2024-02-01 14:43:51 +0530

    Created Container deploy script

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -2,8 +2,7 @@ const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin  = require('webpack/lib/container/ModuleFederationPlugin')
 const commonConfig = require('./webpack.common');
-const packageJson = '../package.json';
-console.log(packageJson);
+const packageJson = require('../package.json');
 const devConfig = {
   mode: 'development',
   devServer: {
@@ -19,9 +18,6 @@ const devConfig = {
         'marketing' : 'marketing@http://localhost:8081/remoteEntry.js'
       },
       shared: packageJson.dependencies
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
     }),
   ],
 };
